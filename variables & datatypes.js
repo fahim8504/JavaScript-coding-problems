96271f9da76321fa88855251057812aee8a55481
Mohamed Fahim <m.mhdfahim@gmail.com>
2024-08-25 10:13:32 +0530

    new files added

---
+++ variables & datatypes.js
@@ -0,0 +1,7 @@
+let course = 'JavaScript';
+const year = 2024;
+let isEnrolled = true;
+
+console.log(typeof course);    // string
+console.log(typeof year);      // number
+console.log(typeof isEnrolled); // boolean
